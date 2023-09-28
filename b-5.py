02ecc0a5fb494346849bc6bf93ac71f89acd9214
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-28 13:48:46 +0900

    関数を一つ一つ実施

--- b-5.py
+++ b-5.py
@@ -1,3 +1,4 @@
+"""
 def calculator():
     numbers = input("データを入力してください(スペース区切り) > ")
     sum_numbers_list = 0
@@ -33,6 +34,55 @@ def calculator():
     # print(f"確認{count}")
 
     print(f"平均値: {sum_numbers_list // count}")
+
+
+calculator()
+"""
+
+
+def get_numbers():
+    numbers = input("データを入力してください(スペース区切り) > ")
+    return [int(num) for num in numbers.split()]
+
+
+def calculate_sum(numbers_list):
+    total = 0
+    for num in numbers_list:
+        total += num
+    return total
+
+
+def calculate_min(numbers_list):
+    min_value = numbers_list[0]
+    for num in numbers_list[1:]:
+        if num < min_value:
+            min_value = num
+    return min_value
+
+
+def calculate_max(numbers_list):
+    max_value = numbers_list[0]
+    for num in numbers_list[1:]:
+        if num > max_value:
+            max_value = num
+    return max_value
+
+
+def calculate_average(numbers_list):
+    total = calculate_sum(numbers_list)
+    count = 0
+    for _ in numbers_list:
+        count += 1
+    return total // count
+
+
+def calculator():
+    numbers_list = get_numbers()
+
+    print(f"合計値: {calculate_sum(numbers_list)}")
+    print(f"最大値: {calculate_max(numbers_list)}")
+    print(f"最小値: {calculate_min(numbers_list)}")
+    print(f"平均値: {calculate_average(numbers_list)}")
 
 
 calculator()
